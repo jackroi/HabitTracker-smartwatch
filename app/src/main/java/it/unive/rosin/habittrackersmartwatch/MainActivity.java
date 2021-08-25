c4a5d43129826286c28b0993e28721cde3746aca
Giacomo Rosin <giacomorosin@gmail.com>
2021-08-25 11:23:34 +0200

    Implement MainActivity and update AndroidManifest (add activities and internet permission

--- app/src/main/java/it/unive/rosin/habittrackersmartwatch/MainActivity.java
+++ app/src/main/java/it/unive/rosin/habittrackersmartwatch/MainActivity.java
@@ -1,23 +1,45 @@
 package it.unive.rosin.habittrackersmartwatch;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.socket.client.Socket;
+import it.unive.rosin.habittrackersmartwatch.api.SessionManager;
 import it.unive.rosin.habittrackersmartwatch.databinding.ActivityMainBinding;
+import it.unive.rosin.habittrackersmartwatch.model.HabitButton;
 
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        SessionManager sessionManager = new SessionManager(this);
+        String token = sessionManager.fetchAuthToken();
 
-        mTextView = binding.text;
+        Class<?> activityToStart;
+        if (token == null) {            // User is non authenticated
+            // Show login activity
+            activityToStart = LoginActivity.class;
+        }
+        else {                          // User is authenticated
+            // Show habit activity
+            activityToStart = HabitActivity.class;
+            HabitSocketApplication app = HabitSocketApplication.getInstance();
+            Socket socket = app.getSocket();
+            socket.connect();
+            socket.emit("online", token);
+        }
+        // Start the activity
+        startActivity(new Intent(this, activityToStart));
+        finish();
     }
+
 }
